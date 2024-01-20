95beb7d9d65826b03a188329e1ce3efe797cec76
Joseandluue <qbpwe@qq.com>
2024-01-20 16:40:46 +0800

    Update trackerslist.py

--- trackerslist.py
+++ trackerslist.py
@@ -34,6 +34,7 @@ merged_content = merge_files(file_contents)
 
 # 添加当前时间到合并结果的第一行，同时在第一行下方添加一个空行
 current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+print(f"当前时间:{current_time}"
 merged_content_with_time = f"更新时间: {current_time}\n\n{merged_content}"
 
 # 保存合并结果到文件
